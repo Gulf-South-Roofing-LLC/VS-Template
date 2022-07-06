bd60f3daa0fcf3139fb2486b5c67505646f438b0
twilightscapes <ihatespam@gmail.com>
2022-07-05 21:14:00 -0500

    font update

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     resolve: "gatsby-plugin-web-font-loader",
     options: {
       custom: {
-        families: ['LibreBaskerville'],
+        families: ['Unpact'],
         urls: ['/assets/fonts/fonts.css'],
       },
     },
